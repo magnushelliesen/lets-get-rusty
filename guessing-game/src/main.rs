b4cfacc23ee998bea2941d61f4fb3a22378f85e1
magnushelliesen <magnus.helliesen@gmail.com>
2026-01-01 14:57:55 +0100

    Complete guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,4 +1,6 @@
 use std::io;
+use std::cmp::Ordering;
+
 use rand::random_range;
 
 fn main() {
@@ -13,6 +15,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low"),
+        Ordering::Equal => println!("Correct"),
+        Ordering::Greater => println!("Too high"),
+    }
 
     println!("You guessed: {guess}");
     println!("Correct number: {secret_number}");
